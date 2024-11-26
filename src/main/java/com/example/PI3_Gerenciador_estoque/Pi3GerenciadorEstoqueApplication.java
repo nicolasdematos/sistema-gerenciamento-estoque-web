4f0aeaaf4feb2b3d7c7dee695dad4497b5c52ddf
Nicolas Matos <112784558+nicolasdematos@users.noreply.github.com>
2024-11-25 21:02:11 -0300

    Primeiro commit do projeto de gerenciamento de estoque

---
+++ src/main/java/com/example/PI3_Gerenciador_estoque/Pi3GerenciadorEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.example.PI3_Gerenciador_estoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pi3GerenciadorEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pi3GerenciadorEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PI3_Gerenciador_estoque/Pi3GerenciadorEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PI3_Gerenciador_estoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pi3GerenciadorEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
